b866e02c8bf367e11a29c43fd166056f00d4a0c2
Kyle McKenna <mckennakyler@gmail.com>
2020-01-29 22:18:22 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,34 @@ require 'pry'
 class String
 
   def sentence?
-    
+    # truefalse if ends with (includes?) a period 
+    if self.include? "."
+      return true
+    else
+      return false
+    end   
   end
 
   def question?
-
+    if self.include? "?"
+      return true
+    else
+      return false
+    end   
   end
 
   def exclamation?
-
+    if self.include? "!"
+      return true
+    else
+      return false
+    end   
   end
 
   def count_sentences
-
+  # Counts the number of sentences stop & start at punctuation (.split [.?!]), make each sentence it's own array, count the number of sentence arrays
+    self.split(/[.?!]+/).grep(/\S/).count 
+    #https://stackoverflow.com/questions/42216136/count-the-number-of-sentences-in-a-paragraph-using-ruby
   end
+  
 end
